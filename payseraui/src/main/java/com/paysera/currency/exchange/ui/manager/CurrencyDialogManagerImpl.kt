c695e56c6af52810012d4f5e52bfab62b3df8cde
Eduardo Delito III <eduardo.delito@gmail.com>
2020-04-15 15:06:13 +0800

    Update javadoc

--- payseraui/src/main/java/com/paysera/currency/exchange/ui/manager/CurrencyDialogManagerImpl.kt
+++ payseraui/src/main/java/com/paysera/currency/exchange/ui/manager/CurrencyDialogManagerImpl.kt
@@ -9,16 +9,29 @@ import javax.inject.Inject
 /**
  * Created by eduardo.delito on 4/15/20.
  */
-class CurrencyDialogManagerImpl @Inject constructor(): CurrencyDialogManager {
-    override fun showDialog(context: Context?, currencyDialogListener: CurrencyDialogListener?, currencies: MutableList<String?>, isReceive: Boolean) {
+class CurrencyDialogManagerImpl @Inject constructor() : CurrencyDialogManager {
+
+    /**
+     *  Show dialog to select available currencies.
+     *  @param context
+     *  @param currencyDialogListener listener for currency UI labels
+     *  @param currencies list of currencies
+     *  @param if dialog shown from receive cta.
+     */
+    override fun showDialog(
+        context: Context?,
+        currencyDialogListener: CurrencyDialogListener?,
+        currencies: MutableList<String?>,
+        isReceive: Boolean
+    ) {
         var selected: String? = ""
         val listItems = currencies.toTypedArray<CharSequence?>()
         val mBuilder = context?.let { AlertDialog.Builder(it) }
         mBuilder?.setTitle(context.getString(R.string.choose_currencies_title))
         mBuilder?.setSingleChoiceItems(listItems, -1) { dialogInterface, i ->
-            dialogInterface.apply { selected = listItems[i].toString()}
+            dialogInterface.apply { selected = listItems[i].toString() }
         }
-        mBuilder?.setPositiveButton(R.string.ok) { dialog, which  ->
+        mBuilder?.setPositiveButton(R.string.ok) { dialog, which ->
             currencyDialogListener?.updateReceiveUI(selected, isReceive)
             dialog.cancel()
             which.or(-1)
